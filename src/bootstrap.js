9e95bdb86be0a13d714cae0464af2535287e11af
marian <hoarra@hotmail.com>
2024-01-29 15:18:52 +0100

    built signinform component for the signin component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Router, Switch, Route } from 'react-router-dom';
+
 import Layout from "./components/layout";
 import reducers from "./reducers";
+import Signin from './components/auth/signin';
+import SignUp from "./components/auth/signup";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import "./style/main.scss";
+import history from './history';
 
 
 function main() {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <Layout>
 
-        </Layout>
-      </BrowserRouter>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <Router history={history}>
+      <Layout>
+        <Switch>
+            <Route path='/' exact component={Signin}/>
+            <Route path='/signin' exact component={Signin}/>
+            <Route path='/signup' exact component={SignUp}/>
+        </Switch>
+
+      </Layout>
+    </Router>
+      
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
