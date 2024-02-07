0191fa4e0c717f84218d143b2b3e217db887c4c6
marian <hoarra@hotmail.com>
2024-02-07 14:39:47 +0100

    connected header and navbar components to redux store to get links

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,24 @@
 import { combineReducers } from 'redux';
-import { reducer as form} from 'redux-form';
+
+import { reducer as form } from 'redux-form'; 
+
+
+
+
+import headerNavbar from './headernavbarReducer';
+
+
+
 
 const rootReducer = combineReducers({
-  form
+
+  form,
+
+  headerNavbar
+
 });
+
+
+
 
 export default rootReducer;
